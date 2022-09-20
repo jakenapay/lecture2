5a5c0407f52e8c00453db8b9ca2357b84ce31c2d
Jake Napay <109712790+Jake-Napay@users.noreply.github.com>
2022-09-21 01:59:58 +0800

    basic updated, conditions added

--- basic.py
+++ basic.py
@@ -1,2 +1,9 @@
+# variable declaration and assigning
 name = "Jake"
-print(f"Hello, {name}, from {name}")
+
+# getting users' input
+getName = input("Name: ")
+
+# printing out and concatenating
+# using formatted strings
+print(f"Hello, {getName}, from {name}")
---
+++ conditions.py
@@ -0,0 +1,11 @@
+# get user integer input
+n = int(input("Number: "))
+
+# else if of python
+# indention is a must
+if n > 0:
+    print(f"{n} is positive number")
+elif n < 0:
+    print(f"{n} is negative number")
+else:
+    print(f"{n} is neither positive nor negative")
