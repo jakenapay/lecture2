e3dfe156669852292f0a0967b8bca11a7245f629
Jake Napay <109712790+Jake-Napay@users.noreply.github.com>
2022-09-21 02:08:55 +0800

    sequences added

---
+++ sequences.py
@@ -0,0 +1,21 @@
+# LISTS - sequence of mutable values / can be modified
+# string
+name = "Raphael"
+
+# print out by index
+print(f"{name[0]}, {name[1]}, {name[2]}, {name[3]}")
+
+# multiple content in list
+names = ["Jake", "Raph", "Raphael", "Ballague"]
+print(names[0])
+
+# TUPLE - opposite of lists
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (coordinateX, coordinateY)
+print(coordinate)
+
+# SET - every value is unique, cannot have the same value inside of it
+
+# DICT - collection of key-value pairs
