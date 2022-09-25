bf4af8d527a958c3299f6060967d0ce38d4d5d93
raphjake <113550034+raphjake@users.noreply.github.com>
2022-09-25 16:50:10 +0800

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -17,7 +17,7 @@ mat = float(input("Enter grade in math: "))
 pro = float(input("Enter grade in programming: "))
 
 totalSub = 4
-ave = (eng + sci + mat + pro) / 4
+ave = (eng + sci + mat + pro) / totalSub
 print(f"Your average: {ave}")
 
 if ave <= 74:
