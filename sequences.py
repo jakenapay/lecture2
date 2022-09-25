c069e70cd1ea0231b446d3cd329e41c4a7d99872
Jake Napay <109712790+Jake-Napay@users.noreply.github.com>
2022-09-25 16:58:21 +0800

    sequences.py is being modified

--- sequences.py
+++ sequences.py
@@ -1,21 +1,28 @@
 # LISTS - sequence of mutable values / can be modified
 # string
-name = "Raphael"
+# name = "Raphael"
 
 # print out by index
-print(f"{name[0]}, {name[1]}, {name[2]}, {name[3]}")
+# output is Raph
+# print(f"{name[0]}, {name[1]}, {name[2]}, {name[3]}")
 
 # multiple content in list
-names = ["Jake", "Raph", "Raphael", "Ballague"]
-print(names[0])
+# names = ["Jake", "Raph", "Raphael", "Ballague"]
+# print(names[0])
 
 # TUPLE - opposite of lists
-coordinateX = 10.0
-coordinateY = 20.0
+# coordinateX = 10.0
+# coordinateY = 20.0
 
-coordinate = (coordinateX, coordinateY)
-print(coordinate)
+# coordinate = (coordinateX, coordinateY)
+# print(coordinate)
 
 # SET - every value is unique, cannot have the same value inside of it
 
 # DICT - collection of key-value pairs
+
+# ====================
+
+# LIST
+foods = ["Banana", "Spaghetti", "Burger", "Pie"]
+print(foods[0])
