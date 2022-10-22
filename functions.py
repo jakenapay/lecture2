aabe8341316df6f78afb048f1845c96428d7421d
Jake Napay <109712790+Jake-Napay@users.noreply.github.com>
2022-10-22 23:43:42 +0800

    functions.py reviewed.

--- functions.py
+++ functions.py
@@ -1,2 +1,20 @@
-def square(x):
-    return x * x
+# EXAMPLE OF FUNCTIONS
+# def square(x):
+#     return x * x
+
+# SIMPLE CELSIUS TO FAHRENHEIT CALCULATOR
+# def fahrenheit(x):
+#     return round((x * 1.8) + 32, 2)
+
+# celsius = float(input("Celsius: "))
+# print(f"Your temperature in fahrenheit: {fahrenheit(celsius)}")
+
+# --------------
+
+# SIMPLE MULTIPLICATION TABLE GENERATOR
+# def multiply(x):
+#     for i in range(1, 11):
+#         print(f"{x} x {i} = {x*i}")
+
+# x = int(input("Enter given number: "))
+# multiply(x)
